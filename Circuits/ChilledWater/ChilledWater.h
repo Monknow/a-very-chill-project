dcb29aa6017acbb97c79cf8d7df7c2ef91046141
Juan <juanrodriguezcode@gmail.com>
2024-11-26 13:54:32 -0600

    fixed (??)

---
+++ Circuits/ChilledWater/ChilledWater.h
@@ -0,0 +1,28 @@
+#ifndef CHILLEDWATER
+#define CHILLEDWATER
+
+#include "../Circuits.h"
+#include "../../Actuator/Chiller/Chiller.h"
+#include "../../Actuator/Pumps/Pumps.h"
+#include <string>
+
+class ChilledWaterCircuit : public Circuit
+{
+public:
+    ChilledWaterCircuit();
+    virtual double get_temperature_transfer_coefficient(double Tcd);
+    int get_Chillers_on_time();
+    void turn_on_one_chiller();
+    void turn_on_both_chillers();
+    void turn_off_both_chillers();
+    int get_total_energy_consuption();
+    int get_total_on_time();
+    int get_chillers_on();
+
+private:
+    Chiller chiller_1;
+    Chiller chiller_2;
+    Pump pumps[8];
+};
+
+#endif
--- comprun.bat
+++ comprun.bat
@@ -1,4 +1,4 @@
 @echo off
-g++ main.cpp .\Actuator\Actuator.cpp .\Actuator\Chiller\Chiller.cpp .\Actuator\FCU\FCU.cpp .\Actuator\Pumps\Pumps.cpp .\Actuator\Tower\Tower.cpp .\Circuits\Circuits.cpp .\Circuits\ChilledWater\ChilledWater.cpp .\Circuits\CondensedWater\ChilledWater.cpp .\Circuits\Recirculating\Recirculating.cpp .\Place\Place.cpp .\Place\Classroom\Classroom.cpp .\Place\Library\Library.cpp -o output
+g++ main.cpp .\Actuator\Actuator.cpp .\Actuator\Chiller\Chiller.cpp .\Actuator\FCU\FCU.cpp .\Actuator\Pumps\Pumps.cpp .\Actuator\Tower\Tower.cpp .\Circuits\Circuits.cpp .\Circuits\ChilledWater\ChilledWater.cpp .\Circuits\CondensedWater\CondensedWater.cpp .\Circuits\Recirculating\Recirculating.cpp .\Place\Place.cpp .\Place\Classroom\Classroom.cpp .\Place\Library\Library.cpp -o output
 
 output.exe
